e06af8c24ed4dc14b7f5970c2db159df8f8c6add
cutls <ryuki_octopus_regist@yahoo.co.jp>
2016-03-01 15:42:58 +0900

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
-function getEndpoint() { return self.registration.pushManager.getSubscription() .then(function(subscription) { if (subscription) { return subscription.endpoint } throw new Error('User not subscribed') }) } self.addEventListener("push", function(event) { event.waitUntil( getEndpoint() .then(function(endpoint) { return fetch('/data.json?endpoint=' + endpoint) }) .then(function(response) { if (response.status === 200) { return response.json() } throw new Error('notification api response error') }) .then(function(response) { self.registration.showNotification(response.title, { icon: response.icon, body: response.body }) }) ) })
+function getEndpoint() {
+  return self.registration.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        return subscription.endpoint
+      }
+      throw new Error('User not subscribed')
+  })
+}
+
+self.addEventListener("push", function(event) {
+  event.waitUntil(
+    getEndpoint()
+    .then(function(endpoint) {
+      return fetch('/notifications.json?endpoint=' + endpoint)
+    })
+    .then(function(response) {
+      if (response.status === 200) {
+        return response.json()
+      }
+      throw new Error('notification api response error')
+    })
+    .then(function(response) {
+      self.registration.showNotification(response.title, {
+        icon: response.icon,
+        body: response.body
+      })
+    })
+  )
+})
